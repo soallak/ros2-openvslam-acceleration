dc97e19910acf852c353b4695ef938387b6fcb25
Saad Ouassil Allak <ouassil@allak.fr>
2023-03-13 20:56:37 +0100

    fix: update repos

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,6 @@ cd $prj_dir
 source ./.envrc
 
 source $ROS2_HOME/setup.bash
-colcon build --merge-install --parallel-workers=32 --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+colcon build --merge-install --parallel-workers=32 --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON #-DROS_VITIS_TARGET=ON
 
 cd $init_dir
